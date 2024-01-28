989a69e804f86b4332da66e26c09ecf13907c237
luxeivan <luxeivan@gmail.com>
2024-01-28 22:24:53 +0300

    commit second

---
+++ src/pages/Services.js
@@ -0,0 +1,80 @@
+import { Card } from 'antd';
+import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import css from './Services.css'
+import categoryB from '../img/categoryB.jpg'
+import categoryBAT from '../img/categoryBAT.jpg'
+import categoryA from '../img/categoryA.jpg'
+import medcom from '../img/medcom.jpg'
+import qvadro from '../img/qvadro.jpg'
+import board from '../img/board.jpg'
+import education from '../img/education.jpg'
+
+const services = [
+    {
+        id: 1,
+        name: "Обучение на категорию «В»",
+        url: "/services/category-b",
+        description: "Обучение на категорию «В»",
+        image: categoryB
+    },
+    {
+        id: 1,
+        name: "Обучение на категорию «В(АТ)»",
+        url: "/services/category-b-at",
+        description: "Обучение на категорию «В(АТ)»",
+        image: categoryBAT
+    },
+    {
+        id: 1,
+        name: "Обучение на категорию «А»",
+        url: "/services/category-b",
+        description: "Обучение на категорию «А»",
+        image: categoryA
+    },
+    {
+        id: 1,
+        name: "Медицинская комиссия",
+        url: "/services/category-b",
+        description: "Медицинская комиссия",
+        image: medcom
+    },
+    {
+        id: 1,
+        name: "Квадроциклы и спецтехника",
+        url: "/services/category-b",
+        description: "Квадроциклы и спецтехника",
+        image: qvadro
+    },
+    {
+        id: 1,
+        name: "Судовождение",
+        url: "/services/category-b",
+        description: "Судовождение",
+        image: board
+    },
+    {
+        id: 1,
+        name: "Обучение и аттестация мастеров",
+        url: "/services/education",
+        description: "Обучение и аттестация мастеров производственного обучения вождению",
+        image: education
+    },
+]
+
+export default function Services() {
+    return (
+        <div className='services'>
+            {services.map(item =>
+                <Link className='services__item' to={item.url}>
+                    <Card className='services__card' title={item.name} bordered={false}
+                        bodyStyle={{ minHeight: "250px", backgroundImage: `url(${item.image})`, backgroundBlendMode: "multiply", backgroundPosition: "bottom right", backgroundSize: "400px", backgroundRepeat: "no-repeat"}}>
+                        {/* <p>{item.description}</p> */}
+                    </Card>
+                </Link>
+            )}
+
+        </div>
+
+    )
+}
---
+++ src/pages/Services/CategoryBAT.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CategoryBAT() {
+  return (
+    <div>CategoryBAT</div>
+  )
+}
